c70ae8a54423c367edb8a64d6f12ecc697b320af
fenrirfx <83608537+fenrirfx@users.noreply.github.com>
2021-07-14 20:16:18 +0100

    Add files via upload

---
+++ Entities/Account.cs
@@ -0,0 +1,38 @@
+﻿using ContaBancaria.Entities.Exceptions;
+
+namespace ContaBancaria.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public double Deposit(double amount)
+        {
+            return amount + Balance;
+        }
+
+        public double Withdraw(double amount)
+        {
+            if (amount > WithdrawLimit)
+            {
+                throw new DomainException("Withdraw Error: The amout exceeds the Withdraw Limit!!");
+            }
+            if (amount > Balance)
+            {
+                throw new DomainException("Withdraw Error: Insuficient Funds!!");
+            }
+            return Balance -= amount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using ContaBancaria.Entities;
+using ContaBancaria.Entities.Exceptions;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the account data: ");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Initial Balance: ");
+                double balance = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw Limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine());
+
+                Account acc1 = new Account(number, name, balance, withdrawLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter the amount to withdraw: ");
+                double amount = double.Parse(Console.ReadLine());
+                acc1.Withdraw(amount);
+                Console.Write("New Balance: " + acc1.Balance);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error! " + e.Message);
+            }
+
+        }
+    }
+}
